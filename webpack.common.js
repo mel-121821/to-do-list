8dd2e691858808f43c1ff2a5c315cdb5d2b47298
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-04-10 14:45:46 -0400

    Rename file

--- webpack.common.js
+++ webpack.common.js
@@ -4,13 +4,17 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
+    // mode: "production",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
+    // devtool: "eval-source-map",
+    // devServer: {
+    //     watchFiles: ["./src/template.html"]
+    // },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html"
@@ -22,6 +26,18 @@ module.exports = {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.html/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource"
+            },
+            {
+               test: /\.(woff|woff2|eot|ttf|otf)$/i,
+               type:"asset/resource"
+            }
         ],
     },
 };
