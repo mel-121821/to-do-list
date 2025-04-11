21de58e751833d605dbbaa182211b1a5f830d079
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-04-11 16:27:23 -0400

    Create dummy task list, export to index.js

--- src/home.js
+++ src/home.js
@@ -1,3 +1,22 @@
 //__________home.js__________
 
 export const test = "JS is working";
+
+const masterTaskList = [
+    {
+        title: "Make grocery list",
+        projectType: "Food",
+        dueDate: "08/04/2025",
+        priority: "low",
+        description: "Need eggs, black forest ham, sliced cheese and english muffins"
+    },
+    {
+        title: "Collect sheets",
+        projectType: "Laundry",
+        dueDate: "08/04/2025",
+        priority: "high",
+        description: "Complete before 5pm"
+    },
+]
+
+export {masterTaskList};
