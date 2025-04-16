aaa2d659ee77df72a75483ee764aa35eb15c12d4
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-04-16 08:46:04 -0400

    Comment out unused code and move to bottom of page - no longer using different functions for collapsed vs. expanded views. Change loop to also include index, add event listeners for expand and delete btns, add dataset on taskDivs and set to index, create function to collapse and expand tasks

--- src/index.js
+++ src/index.js
@@ -11,8 +11,21 @@ console.log(masterTaskList);
 
 // for rendering
 function expandTaskToFullView(obj) {
-    
+    // 
 }
+
+
+
+// function showIndex (obj) {
+//     const expandBtn = document.querySelector("#content > div");
+//     expandBtn.addEventListener("click", 
+//         console.log("expand button clicked")
+//     );
+// }
+
+
+
+// expandTaskToFullView(masterTaskList)
 
 function renderTaskList(obj) {
     const content = document.querySelector("#content");
@@ -55,7 +68,7 @@ function renderTaskList(obj) {
 function renderTaskListDev (obj) {
     // create elements
     const content = document.querySelector("#content");
-    for (const task of obj) {
+    obj.forEach((task, index) => {
         const taskDiv = document.createElement("div");
         const taskCheckbox = document.createElement("input");
         taskCheckbox.setAttribute("type", "checkbox");
@@ -77,6 +90,16 @@ function renderTaskListDev (obj) {
         const saveBtn = document.createElement("button");
         const cancelBtn = document.createElement("button");
 
+        // event listeners
+        expandBtn.addEventListener("click", function(e) { console.log("Expand/Collapse btn clicked");
+        console.log(taskDiv.dataset.index)
+        collapseTask(taskDiv);
+        });
+
+        deleteBtn.addEventListener("click", function(e) {
+            console.log("Delete btn clicked");
+        })
+
         // populate info
         title.textContent = `${task.title}`;
         expandBtn.textContent = "exp";
@@ -96,11 +119,51 @@ function renderTaskListDev (obj) {
         userChecklistDiv.append(userChecklist, btnAddChecklistItem);
         btnDiv.append(saveBtn, cancelBtn)
         taskDiv.append(taskCheckbox, title, expandBtn, deleteBtn, taskCategories, description, userChecklistDiv, btnDiv);
+        taskDiv.classList.add("expanded")
+        taskDiv.dataset.index = index;
         content.appendChild(taskDiv);
-        content.classList.add("expanded")
-    }
+    })  
 }
 
 
-renderTaskList(masterTaskList);
-// renderTaskListDev(masterTaskList);
+// renderTaskList(masterTaskList);
+renderTaskListDev(masterTaskList);
+
+
+function collapseTask(taskDiv) {
+    taskDiv.classList.toggle("active");
+    const description = taskDiv.children.item(5);
+    const userChecklistDiv = taskDiv.children.item(6)
+    if (description.style.display !== "none" && userChecklistDiv.style.display !== "none") {
+        description.style.display = "none"
+        userChecklistDiv.style.display = "none"
+    } else {
+        description.style.display = "block"
+        userChecklistDiv.style.display = "flex"
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// const expBtns = document.querySelectorAll(".expand");
+// console.log(expBtns);
+// console.log(typeof(expBtns))
+// for (const expBtn of expBtns) {
+//     expBtn.addEventListener("click", function(e) {
+//         getIndexOfTask(expBtns)
+//         console.log("expand button clicked")
+//     });
+// }
+
+// function getIndexOfTask(taskList, index) {
+//     console.log(index);
+// }
