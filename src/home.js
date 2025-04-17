cd127e72cc6ef2f4603bd274b4bafc85f36628e2
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-04-17 10:38:08 -0400

    Create fn to read project categories from masterTaskList, export to index.js

--- src/home.js
+++ src/home.js
@@ -5,25 +5,37 @@ export const test = "JS is working";
 const masterTaskList = [
     {
         title: "Make grocery list",
-        projectType: "Food",
+        category: "Food",
         dueDate: "08/04/2025",
         priority: "low",
         description: "Need eggs, black forest ham, sliced cheese and english muffins"
     },
     {
         title: "Collect sheets",
-        projectType: "Laundry",
+        category: "Laundry",
         dueDate: "08/04/2025",
         priority: "high",
         description: "Complete before 5pm"
     },
     {
         title: "Wash cat dishes",
-        projectType: "Pets",
+        category: "Pets",
         dueDate: "08/04/2025",
         priority: "normal",
         description: "Wash both water bowls, plus automatic feeder and refill. Mix proper ratio of foods: 3 parts HP, 1 part dental and 1 part gastro."
     }
 ]
 
-export {masterTaskList};
+function getCategories(masterTaskList) {
+    const allProjectCategories = [];
+    for (const task of masterTaskList) {
+        console.log(task.category)
+        allProjectCategories.push(`${task.category}`)
+    }
+    return allProjectCategories
+}
+
+
+
+export{masterTaskList};
+export {getCategories};
