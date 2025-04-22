5319bab35c3240f479c1eb7cc8cfdf7453286b6c
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-04-22 07:51:44 -0400

    Add userChecklist arrays to masterTaskList

--- src/home.js
+++ src/home.js
@@ -1,35 +1,45 @@
 //__________home.js__________
 
-export const test = "JS is working";
-
 const masterTaskList = [
     {
         title: "Make grocery list",
         category: "Food",
         dueDate: "2025-04-09",
         priority: "low",
-        description: "Need eggs, black forest ham, sliced cheese and english muffins"
+        description: "Need eggs, black forest ham, sliced cheese and english muffins",
+        userChecklist: [
+            "Eggs", "BFH, 400g shaved", "Havarti with jalepeno", "Eng muffin (white)", "Also need garlic mayo"
+        ]
     },
     {
-        title: "Collect sheets",
+        title: "Do Laundry",
         category: "Laundry",
         dueDate: "2025-04-18",
         priority: "high",
-        description: "Complete before 5pm"
+        description: "Laundromat opens at 6am",
+        userChecklist: [
+            "Go to the bank to get change"
+        ]
     },
     {
-        title: "Wash cat dishes",
+        title: "Cat Chores",
         category: "Pets",
         dueDate: "2025-04-16",
         priority: "normal",
-        description: "Wash both water bowls, plus automatic feeder and refill. Mix proper ratio of foods: 3 parts HP, 1 part dental and 1 part gastro."
+        description: "Wash both water bowls, plus automatic feeder and refill. Mix proper ratio of foods: 3 parts HP, 1 part dental and 1 part gastro.",
+        userChecklist: [
+            "Wash water bowls", "Rotate auto feeder, wash and refill", "Clean litter boxes", "Put cat beds in laundry basket"
+        ]
     },
     {
         title: "Make dinner",
         category: "Food",
         dueDate: "2025-04-09",
         priority: "low",
-        description: "Take meat out of the freezer by noon"
+        description: "Menu: Udon stir-fry",
+        userChecklist: [
+            "Defrost beef strips", "Marinate strips with 1/2 sauce for a few hours", "Blanche udon and broccoli", "Prep onion and garlic", "Saute onion garlic and beef", "Add broccoli and udon", "Add remaining sauce", "Serve"
+        ]
     },
 ]
 
@@ -60,13 +70,13 @@ function getCategories(categories) {
     return allProjectCategories;
 }
 
-const priorityOptions = ["low", "normal", "high"]
+const priorities = ["low", "normal", "high"]
 
-const getPriorities = () => priorityOptions;
+const getPriorities = () => priorities;
 
 export{masterTaskList};
 export {categories}
 export {getCategories};
 
-export {priorityOptions}
+export {priorities}
 export {getPriorities}
