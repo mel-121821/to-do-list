8d1025ea736e42b108b787b2752c0b18c6efe1e9
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-05-23 13:51:38 -0400

    Remove unused code and move deleteProject() over from home.js as it is related to projects. Create a fn() to add a project and import pubsub so that emit can be called on add/delete fn()s

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,26 @@
  //__________Projects Logic__________
  
+ import { pubSub } from "./pubsub.js";
+ 
  const projectManager = (function() {
+
+    function Project(name){
+        this.name = name
+    }
+
+    function addProject(name) {
+        projects.push(new Project(name))
+        pubSub.emit("project")
+    }
+
+    function deleteProject() {
+        const index = this.parentNode.dataset.index
+        projects.splice(index, 1)
+        console.log(projectManager.getProjects())
+        // moveProjectsToAll()
+        pubSub.emit("projectDeleted", projects)
+    }
+
     const projects = [
         {
             icon: "all-icon",
@@ -22,13 +42,24 @@
 
      // filter out duplicates
     const projectArr = projects.map(function(project){
-        return project.name
+        return project
     })
 
-    const getProjects = () => projectArr
+    const getProjects = () => projects
+
+    const projectNamesArr = projects.map(((project) => project.name))
+      
+
+    const getProjectNames = () => projectNamesArr
+    // console.log(projectNamesArr)
+
+    
 
     return {
+        addProject,
+        deleteProject,
         getProjects,
+        getProjectNames,
     }
 })()
 
