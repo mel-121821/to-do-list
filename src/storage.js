0cdf0dc71d6aa733948ea525068f7cbe6b8e552a
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-07-07 13:06:41 -0400

    Remove unused code and console.logs, add semi- colons

--- src/storage.js
+++ src/storage.js
@@ -2,113 +2,65 @@
 
 const storage = (function() {
 
-    // test for local storage is taken from MDN web storage documentation. For more info, visit:
-    // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
-
-    function storageAvailable(type) {
-        let storage;
-        try {
-            storage = window[type];
-            const x = "__storage_test__";
-            storage.setItem(x, x);
-            storage.removeItem(x);
-            return true;
-        } catch (e) {
-            return (
-            e instanceof DOMException &&
-            e.name === "QuotaExceededError" &&
-            // acknowledge QuotaExceededError only if there's something already stored
-            storage &&
-            storage.length !== 0
-            );
-        }
-    }
-
-    function testLocalStorage() {
-    if (storageAvailable("localStorage")) {
-        console.log("Yippee! We can use localStorage awesomeness")
-        } else {
-        console.log("Too bad, no localStorage for us")
-        }
-    }
-
-// ___________My code____________
-
     function checkTasksExist() {
         if (!localStorage.getItem("tasks") || getStoredTasks().length < 1) {
-            return false
+            return false;
         } else {
-            console.log("tasks exist in local storage")
-            return true
+            return true;
         }
     }
 
     function getStoredTasks() {
-        let tasks = JSON.parse(localStorage.getItem("tasks"))
-        return tasks
+        let tasks = JSON.parse(localStorage.getItem("tasks"));
+        return tasks;
     }
 
 
     function checkProjectsExist() { 
         if (!localStorage.getItem("projects")) {
-            console.log("projects do not exist in local storage, return false")
-            return false
+            return false;
         } else {
-            console.log("projects exist in local storage")
-            return true
+            return true;
         }
     }
 
     function getStoredProjects() {
-        let projects = JSON.parse(localStorage.getItem("projects"))
-        console.log(projects)
-        return projects
+        let projects = JSON.parse(localStorage.getItem("projects"));
+        return projects;
     }
 
     function checkThemesExist() {
         if (!localStorage.getItem("themes")) {
-            console.log("No themes in local storage")
-            return false
+            return false;
         } else {
-            console.log("Themes exist in local storage")
-            return true
+            return true;
         }
     }
 
     function getStoredThemes() {
-        let themes = JSON.parse(localStorage.getItem("themes"))
-        console.log(themes)
-        return themes
+        let themes = JSON.parse(localStorage.getItem("themes"));
+        return themes;
     }
 
-    // function populateStorage(key, data) {
-    //     localStorage.setItem(key, JSON.stringify(data))
-    // }
-
     function storeTasks(data) {
-        localStorage.setItem("tasks", JSON.stringify(data))
-        console.log(JSON.parse(localStorage.getItem("tasks")))
+        localStorage.setItem("tasks", JSON.stringify(data));
     }
 
     function storeProjects(data) {
-        localStorage.setItem("projects", JSON.stringify(data))
-        console.log(JSON.parse(localStorage.getItem("projects")))
+        localStorage.setItem("projects", JSON.stringify(data));
     }
 
     function storeThemes(data) {
-        localStorage.setItem("themes", JSON.stringify(data))
-        console.log(JSON.parse(localStorage.getItem("themes")))
+        localStorage.setItem("themes", JSON.stringify(data));
     }
 
     function clearLocalStorage() {
-        localStorage.clear()
+        localStorage.clear();
     }
 
     
 
     return {
-        testLocalStorage,
-
         checkTasksExist,
         getStoredTasks,
         storeTasks,
@@ -123,6 +75,6 @@ const storage = (function() {
 
         clearLocalStorage,
     }
-})() 
+})(); 
 
-export { storage }
+export { storage };
